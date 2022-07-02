4f19557c516e81313bcad256d8188800bc2c4bdb
Ivan Notaros <nothke@gmail.com>
2022-07-02 17:05:09 +0200

    Mussel push with both keys

--- Assets/Scripts/MusselController.cs
+++ Assets/Scripts/MusselController.cs
@@ -7,14 +7,46 @@ public class MusselController : MonoBehaviour
     public Transform upShell;
     public Transform downShell;
 
+
+
     public float maxAngle = 30;
+
+    Rigidbody _rb;
+    Rigidbody rb { get { if (!_rb) _rb = GetComponent<Rigidbody>(); return _rb; } }
+
+    float timeSinceLastLeft;
+    float timeSinceLastRight;
+
+    float thrust = 0;
 
     void Update()
     {
-        float targetLeft = Input.GetKey(KeyCode.A) ? 1 : 0;
-        float targetRight = Input.GetKey(KeyCode.D) ? 1 : 0;
+        KeyCode leftKey = KeyCode.A;
+        KeyCode rightKey = KeyCode.D;
+
+        float targetLeft = Input.GetKey(leftKey) ? 1 : 0;
+        float targetRight = Input.GetKey(rightKey) ? 1 : 0;
+
+        float time = Time.time;
+
+        if (Input.GetKeyDown(leftKey))
+            timeSinceLastLeft = time;
+
+        if (Input.GetKeyDown(rightKey))
+            timeSinceLastRight = time;
+
+
+
+        if (time - timeSinceLastLeft < 0.5f &&
+            time - timeSinceLastRight < 0.5f)
+            thrust = 1;
+
+        thrust -= Time.deltaTime;
+        thrust = Mathf.Clamp01(thrust);
 
         upShell.localEulerAngles = new Vector3(-maxAngle * (1 - targetLeft), 0, 0);
         downShell.localEulerAngles = new Vector3(maxAngle * (1 - targetRight), 0, 0);
+
+        rb.AddForce(-transform.forward * 10f * thrust, ForceMode.Acceleration);
     }
 }
