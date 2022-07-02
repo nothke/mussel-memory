e675c692674b9df4572dddcf821f433f132b1dfa
Ivan Notaros <nothke@gmail.com>
2022-07-02 22:43:02 +0200

    Left right shell

--- Assets/Scripts/MusselController.cs
+++ Assets/Scripts/MusselController.cs
@@ -7,69 +7,82 @@ public class MusselController : MonoBehaviour
     public Transform upShell;
     public Transform downShell;
 
-    public float maxAngle = 30;
+    public const float maxAngle = 30;
 
     Rigidbody _rb;
     Rigidbody rb { get { if (!_rb) _rb = GetComponent<Rigidbody>(); return _rb; } }
 
-    float timeSinceLastLeft;
-    float timeSinceLastRight;
-
     float thrust = 0;
     public float thrustMult = 1;
-
     float smooth = 0.1f;
-
     public float torqueMult = 1.0f;
 
     [System.Serializable]
     public class Shell
     {
+        public Rigidbody rb;
+
+        public KeyCode thrustKey;
+        public float side = 1;
+
         public Transform t;
         public float timeSinceLast;
+
+        public Transform forcePoint;
+
+        public float open = 0;
+        public float velo = 0;
+
+        public bool thrusting = false;
+        public float thrust;
+
+        const float smooth = 0.1f;
+
+
+
+        public void Update()
+        {
+            float time = Time.time;
+
+            float target = Input.GetKey(thrustKey) ? 1 : 0;
+
+            if (Input.GetKeyDown(thrustKey))
+                timeSinceLast = time;
+
+            thrusting = time - timeSinceLast < 0.3f && Input.GetKey(thrustKey);
+            thrust = thrusting ? 1 : 0;
+
+            float factor = (time - timeSinceLast) / 0.3f;
+
+            open = Mathf.SmoothDamp(open, target, ref velo, smooth);
+
+            t.localEulerAngles = new Vector3(maxAngle * (1 - open) * side, 0, 0);
+
+            if (thrusting)
+                rb.AddForceAtPosition(t.parent.forward * (thrust * 10 * factor), forcePoint.position, ForceMode.Acceleration);
+        }
     }
 
-    float leftOpen = 0;
-    float leftVelo = 0;
-    float rightOpen = 0;
-    float rightVelo = 0;
+    public Shell left;
+    public Shell right;
+
 
     void Update()
     {
-        KeyCode leftKey = KeyCode.A;
-        KeyCode rightKey = KeyCode.D;
+        right.Update();
+        left.Update();
 
-        float targetLeft = Input.GetKey(leftKey) ? 1 : 0;
-        float targetRight = Input.GetKey(rightKey) ? 1 : 0;
-
-        float time = Time.time;
-
-        if (Input.GetKeyDown(leftKey))
-            timeSinceLastLeft = time;
-
-        if (Input.GetKeyDown(rightKey))
-            timeSinceLastRight = time;
-
-        bool thrustingLeft = time - timeSinceLastLeft < 0.3f && Input.GetKey(leftKey);
-        bool thrustingRight = time - timeSinceLastRight < 0.3f && Input.GetKey(rightKey);
-
-        float thrustLeft = thrustingLeft ? 1 : 0;
-        float thrustRight = thrustingRight ? 1 : 0;
-
-        if (thrustingLeft && thrustingRight)
-            thrust = 1;
+        //if (left.thrusting && right.thrusting)
+        //    thrust = 1;
 
         thrust -= Time.deltaTime;
         thrust = Mathf.Clamp01(thrust);
 
-        leftOpen = Mathf.SmoothDamp(leftOpen, targetLeft, ref leftVelo, smooth);
-        rightOpen = Mathf.SmoothDamp(rightOpen, targetRight, ref rightVelo, smooth);
 
-        upShell.localEulerAngles = new Vector3(-maxAngle * (1 - leftOpen), 0, 0);
-        downShell.localEulerAngles = new Vector3(maxAngle * (1 - rightOpen), 0, 0);
+        //rb.AddRelativeTorque(
+        //    Vector3.right * (left.thrust - right.thrust)
+        //    * torqueMult, ForceMode.Acceleration);
 
-        rb.AddRelativeTorque(Vector3.right * (thrustLeft - thrustRight) * torqueMult, ForceMode.Acceleration);
-
-        rb.AddForce(-transform.forward * 10f * thrust * thrustMult, ForceMode.Acceleration);
+        //rb.AddForce(transform.forward * 10f * thrust * thrustMult, ForceMode.Acceleration);
     }
 }
