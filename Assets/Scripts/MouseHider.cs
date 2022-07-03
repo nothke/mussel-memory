599555373d22cd2350409b41c2a9b01c3acb09fd
Ivan Notaros <nothke@gmail.com>
2022-07-03 03:52:13 +0200

    Update emit.mat, emit.mat.meta, and 47 more files...

---
+++ Assets/Scripts/MouseHider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHider : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
